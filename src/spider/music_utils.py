6c7c0be39bc3ab694ebd19ebc5e196a8ba5cec98
Silver <silver.codingcat@gmail.com>
2018-01-23 22:42:58 +0800

    Add regular

--- src/spider/music_utils.py
+++ src/spider/music_utils.py
@@ -2,6 +2,7 @@
 """
 Common method for music
 """
+import re
 
 
 def match_song_id(url):
@@ -9,6 +10,7 @@ def match_song_id(url):
     Get song id from url
     https://music.163.com/song?id=11111111&userid=11111111
     """
+    pattern = 'song\?id=\d+'
     pass
 
 
@@ -17,4 +19,5 @@ def match_playlist_id(url):
     Get playlist id from url
     https://music.163.com/playlist?id=11111111&userid=11111111
     """
+    pattern = 'playlist\?id=\d+'
     pass
