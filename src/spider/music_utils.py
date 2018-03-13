a3b64114efdeb525642b2d0e146537a72b05eb68
Silver <silver.codingcat@gmail.com>
2018-03-13 23:26:20 +0800

    Add match type

--- src/spider/music_utils.py
+++ src/spider/music_utils.py
@@ -5,6 +5,25 @@ Common method for music
 import re
 
 
+def match_type(url):
+    """
+    Get id type in url.
+    Song id: 0b001
+    Playlist id: 0b010
+    User id: 0b100
+    :param url:
+    :return:The bit signal of type
+    """
+    id_type = 0b0
+    if match_user_id(url) is not None:
+        id_type = id_type | 0b100
+    if match_song_id(url) is not None:
+        id_type = id_type | 0b001
+    elif match_playlist_id(url) is not None:
+        id_type = id_type | 0b010
+    return id_type
+
+
 def match_song_id(url):
     """
     Get song id from url
@@ -37,6 +56,7 @@ def match_id(pattern, url):
     """
     Get id from url
     """
+
     def in_match(pattern, value):
         """
         Match value
@@ -44,6 +64,7 @@ def match_id(pattern, url):
         match = re.search(pattern, value)
         match = None if not match else match.group()
         return match
+
     matched_id = in_match(pattern, url)
     if not matched_id:
         return None
