d9ed597acf0248bb91b5be60f2a9299f51b57057
Silver <silver.codingcat@gmail.com>
2018-04-22 18:06:21 +0800

    Update utils id type, painter

--- src/avatar_painter.py
+++ src/avatar_painter.py
@@ -3,14 +3,18 @@
 Download song playlist album picture.
 """
 from spider.music_spider import MusicSpider
+import spider.music_utils as utils
 import requests
+import os
 
 
 class Avatar(object):
-    def __init__(self, id, url):
-        self.id = id
+    def __init__(self, avatar_id, url, avatar_type):
+        self.avatar_id = avatar_id
         self.url = url
+        self.avatar_type = avatar_type
         self.extension = 'jpg'
+        self.short_name = '{0}_{1}.{2}'.format(avatar_type, avatar_id, self.extension)
 
 
 class AvatarPainter(object):
@@ -22,16 +26,33 @@ class AvatarPainter(object):
     def album(self, album_id):
         content = self.spider.request_album(album_id)
         pic_url = content['album']['picUrl']
-        avatar = Avatar(album_id, pic_url)
-        return avatar
+        return Avatar(album_id, pic_url, 'album')
 
     def playlist(self, playlist_id):
         content = self.spider.request_playlist(playlist_id)
         pic_url = content['playlist']['coverImgUrl']
-        avatar = Avatar(playlist_id, pic_url)
-        return avatar
+        return Avatar(playlist_id, pic_url, 'playlist')
 
-    def paint(self, avatar):
-        pic_name = '{0}/{1}.{2}'.format(self.path, avatar.id, avatar.extension)
+    def artist(self, artist_id):
+        content = self.spider.request_artist(artist_id)
+        pic_url = content['artist']['picUrl']
+        return Avatar(artist_id, pic_url, 'artist')
+
+    def download(self, avatar):
+        pic_name = '{0}/{1}'.format(self.path, avatar.short_name)
+        if not os.path.isdir(self.path):
+            os.makedirs(self.path)
         with open(pic_name, 'wb') as pic_file:
             pic_file.write(requests.get(avatar.url).content)
+
+    def paint(self, url):
+        url_type = utils.match_type(url)
+        if url_type & 0b00100 == 0b00100:
+            avatar = self.playlist(utils.match_playlist_id(url))
+        elif url_type & 0b01000 == 0b01000:
+            avatar = self.album(utils.match_album_id(url))
+        elif url_type & 0b10000 == 0b10000:
+            avatar = self.artist(utils.match_artist_id(url))
+        else:
+            return
+        self.download(avatar)
--- src/spider/music_utils.py
+++ src/spider/music_utils.py
@@ -8,20 +8,36 @@ import re
 def match_type(url):
     """
     Get id type in url.
-    Song id: 0b001
-    Playlist id: 0b010
-    User id: 0b100
+    User:     0b00001
+    Song:     0b00010
+    Playlist: 0b00100
+    Album:    0b01000
+    Artist:   0b10000
     :param url:
     :return:The bit signal of type
     """
     id_type = 0b0
-    if match_user_id(url) is not None:
-        id_type = id_type | 0b100
-    if match_song_id(url) is not None:
-        id_type = id_type | 0b001
-    elif match_playlist_id(url) is not None:
-        id_type = id_type | 0b010
+    if match_user_id(url):
+        id_type = id_type | 0b00001
+    if match_song_id(url):
+        id_type = id_type | 0b00010
+    elif match_playlist_id(url):
+        id_type = id_type | 0b00100
+    elif match_album_id(url):
+        id_type = id_type | 0b01000
+    elif match_artist_id(url):
+        id_type = id_type | 0b10000
     return id_type
+
+
+def match_user_id(url):
+    """
+    Get user id from url
+    https://music.163.com/song?id=11111111&userid=11111111
+    https://music.163.com/playlist?id=11111111&userid=11111111
+    """
+    pattern = r'userid=\d+'
+    return match_id(pattern, url)
 
 
 def match_song_id(url):
@@ -42,13 +58,21 @@ def match_playlist_id(url):
     return match_id(pattern, url)
 
 
-def match_user_id(url):
+def match_album_id(url):
     """
-    Get user id from url
-    https://music.163.com/song?id=11111111&userid=11111111
-    https://music.163.com/playlist?id=11111111&userid=11111111
+    Get album id from url
+    http://music.163.com/album?id=11111&userid=11111111
     """
-    pattern = r'userid=\d+'
+    pattern = r'album\?id=\d+'
+    return match_id(pattern, url)
+
+
+def match_artist_id(url):
+    """
+    Get artist id from url
+    http://music.163.com/#/artist?id=11111
+    """
+    pattern = r'artist\?id=\d+'
     return match_id(pattern, url)
 
 
