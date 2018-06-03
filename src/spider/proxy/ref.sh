7e96343212178bc4bb68c8b30d50c30ebff10fb7
Silver <silver.codingcat@gmail.com>
2018-06-04 01:57:39 +0800

    Remove redundance file

--- src/spider/proxy/ref.sh
+++
@@ -1,4 +0,0 @@
-pip install sqlalchemy
-pip install requests
-pip install bs4
-pip install threadpool
--- src/spider/ref.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pip install future
-pip install pycryptodome
-pip install BeautifulSoup4
-pip install requests
-pip install threadpool
-pip install sqlalchemy
-pip install mysql-connector
